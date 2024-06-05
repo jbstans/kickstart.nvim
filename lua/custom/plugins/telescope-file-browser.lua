125fae6befcd53ce588773e45796a06171a8b5c9
Jack Beauchamp-Stansfield <jbeauchamp-stansfield@f1.com>
2024-06-05 15:12:03 +0100

    Added telescope filebrowser and keymaps

---
+++ lua/custom/plugins/telescope-file-browser.lua
@@ -0,0 +1,8 @@
+local telescope_file_browser = {
+  {
+    'nvim-telescope/telescope-file-browser.nvim',
+    dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
+  },
+}
+
+return { telescope_file_browser }
