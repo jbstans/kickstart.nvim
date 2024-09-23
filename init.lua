2df740c1c8d5542f6f6d463240fbe23b32deea9c
Jack Beauchamp-Stansfield <jbstans@jbstans.co.uk>
2024-09-23 10:43:42 +0100

    Fixed markdownlint crash and added a markdown formatter

--- init.lua
+++ init.lua
@@ -846,7 +846,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'python', 'luadoc', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -1,13 +1,9 @@
-local keymaps = {
+return {
   vim.keymap.set('n', '<space>fb', ':Telescope file_browser<CR>'),
-
   -- open file_browser with the path of the current buffer
   vim.keymap.set('n', '<space>fb', ':Telescope file_browser path=%:p:h select_buffer=true<CR>'),
-
   -- Alternatively, using lua API
   vim.keymap.set('n', '<space>fb', function()
     require('telescope').extensions.file_browser.file_browser()
   end),
 }
-
-return { keymaps }
